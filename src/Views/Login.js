fcf1e8e0175c89c4529989bb47489d9e777cea10
Daniel Escobar <daescobarp@unal.edu.co>
2020-06-12 21:44:26 -0500

    login working

--- src/Views/Login.js
+++ src/Views/Login.js
@@ -1,6 +1,74 @@
-import React from "react";
-import { Typography } from "@material-ui/core";
+import React, { useState } from "react";
+import { Typography, Grid, Button } from "@material-ui/core";
+import { ValidatorForm, TextValidator } from "react-material-ui-form-validator";
+import { makeStyles } from "@material-ui/core/styles";
 import { withRouter } from "react-router-dom";
-export default withRouter(function Login() {
-  return <Typography>Login</Typography>;
+const useStyles = makeStyles({
+  title: {
+    textAlign: "center",
+  },
+  form: {
+    display: "flex",
+    flexDirection: "column",
+  },
+  textInput: {
+    marginTop: "5%",
+  },
+});
+
+export default withRouter(() => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const handleUsername = (e) => {
+    e.preventDefault();
+    setUsername(e.target.value);
+  };
+  const handlePassword = (e) => {
+    e.preventDefault();
+    setPassword(e.target.value);
+  };
+  const handleSubmit = () => {
+    console.log([username, password]);
+  };
+  const classes = useStyles();
+  return (
+    <Grid container direction="column">
+      <Typography variant="h6" className={classes.title}>
+        Bienvenidos al formulario de ingreso a laboratorios
+      </Typography>
+      <img alt="lab" width="100%" src={require("./lab.svg")} />
+      <ValidatorForm className={classes.form} onSubmit={handleSubmit}>
+        <TextValidator
+          className={classes.textInput}
+          label="Usuario Institucional"
+          onChange={handleUsername}
+          name="username"
+          value={username}
+          autoComplete="off"
+          validators={["required"]}
+          errorMessages={["Este campo es requerido"]}
+          variant="outlined"
+        />
+        <TextValidator
+          className={classes.textInput}
+          label="Contraseña"
+          onChange={handlePassword}
+          name="password"
+          type="password"
+          variant="outlined"
+          validators={["required"]}
+          errorMessages={["Este campo es requerido"]}
+          value={password}
+        />
+        <Button
+          type="submit"
+          variant="contained"
+          color="primary"
+          className={classes.textInput}
+        >
+          Ingresar
+        </Button>
+      </ValidatorForm>
+    </Grid>
+  );
 });
