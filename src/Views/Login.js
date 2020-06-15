554c9960346cc79d0371a6c38d75b32a5e4ec067
Daniel Escobar <daescobarp@unal.edu.co>
2020-06-14 22:26:20 -0500

    connected login with backend

--- src/Views/Login.js
+++ src/Views/Login.js
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
-import { Typography, Grid, Button } from "@material-ui/core";
+import { Typography, Grid, Button, Snackbar } from "@material-ui/core";
+import { Alert } from "@material-ui/lab";
 import { ValidatorForm, TextValidator } from "react-material-ui-form-validator";
 import { makeStyles } from "@material-ui/core/styles";
 import { withRouter } from "react-router-dom";
+import Backend from "../serviceBackend";
 const useStyles = makeStyles({
   title: {
     textAlign: "center",
@@ -19,6 +21,8 @@ const useStyles = makeStyles({
 export default withRouter((props) => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [showSnack, setShowSnack] = useState(false);
+  const [snackProp, setSnackProp] = useState("");
   const handleUsername = (e) => {
     e.preventDefault();
     setUsername(e.target.value);
@@ -28,8 +32,19 @@ export default withRouter((props) => {
     setPassword(e.target.value);
   };
   const handleSubmit = () => {
-    console.log([username, password]);
-    props.history.push("/form");
+    Backend.sendLogin(username, password).then(async (res) => {
+      if (res.status === 200) {
+        let resJSON = await res.json();
+        localStorage.setItem("jwt", resJSON.token);
+        props.history.push("/form");
+      } else if (res.status === 203) {
+        setSnackProp("Usuario o contraseña incorrectos");
+        setShowSnack(true);
+      } else {
+        setSnackProp("Error en el servidor");
+        setShowSnack(true);
+      }
+    });
   };
   const classes = useStyles();
   return (
@@ -70,6 +85,24 @@ export default withRouter((props) => {
           Ingresar
         </Button>
       </ValidatorForm>
+
+      <Snackbar
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "center",
+        }}
+        open={showSnack}
+        autoHideDuration={6000}
+        onClose={() => setShowSnack(false)}
+      >
+        <Alert
+          onClose={() => setShowSnack(false)}
+          severity="error"
+          variant="filled"
+        >
+          {snackProp}
+        </Alert>
+      </Snackbar>
     </Grid>
   );
 });
